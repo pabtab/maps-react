e9df2ecbddbd294cdf7683c4d72a289c980c009b
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-02-24 18:58:37 -0500

    google map library added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import GoogleMapReact from 'google-map-react';
+
 class App extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   render() {
     return (
       <div className="App">
@@ -11,14 +21,20 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: 'AIzaSyAKjju9A0WMwGN-VyhEyQosNS4x92Q5_Kc' }}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+            >
+              {/* <AnyReactComponent
+                lat={59.955413}
+                lng={30.337844}
+                text="My Marker"
+              /> */}
+            </GoogleMapReact>
+          </div>
         </header>
       </div>
     );
